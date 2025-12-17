721e5a40aa8c1aa0e0e0e1cf9e262c9b2e48d608
noodleman1991 <149662590+noodleman1991@users.noreply.github.com>
2025-12-18 01:41:17 +0200

    Update main page layout

--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { StackHandler } from "@stackframe/stack";
 
 export default function Handler() {
-  return <StackHandler fullPage />;
+  return (
+    <Suspense>
+        <StackHandler fullPage />;
+    </Suspense>
+  );
 }
