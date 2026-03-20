59e188940577582782838d388a753d6e4f570b27
noodleman1991 <149662590+noodleman1991@users.noreply.github.com>
2026-03-20 01:32:16 +0100

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,5 +4,9 @@ import { env } from "@/env.js";
 
 import * as schema from "./schema";
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(env.DATABASE_URL, {
+  max: 20,
+  idle_timeout: 30,
+  connect_timeout: 10,
+});
 export const db = drizzle(client, { schema });
